5fd795b7e589e5506112d5990e5e0ff50b8944bb
Tonio Loewald <tonio@loewald.com>
2019-02-02 15:07:57 -0800

    migrated to import

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const url = require('url');
 
 const settings = {
-  socket: 8018,
+  socket: 8444,
   web_root: __dirname,
   cert_path: 'localhost-ssl/public.pem',
   key_path: 'localhost-ssl/private.pem',
